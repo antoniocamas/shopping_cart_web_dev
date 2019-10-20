1636b00100a02881f519cc7e733b62a817cfa5fe
Antonio Camas <antoniocamas@hotmail.com>
2019-10-21 00:08:02 +0200

    Add dynamic Item input fields

--- src/main/resources/static/js/behavior.js
+++ src/main/resources/static/js/behavior.js
@@ -1,5 +1,21 @@
 function deleteOrderConfirmation(delete_location) {
-    if(confirm("Are you sure you what to delete this entry?")){
+    if(confirm("Are you sure you want to delete this entry?")){
         document.location = delete_location;
     }    
 }
+
+$(document).ready(function() {
+    $("#addItem").click(function() {
+        
+        
+        //var lastField = $("#itemTextBoxes");
+        //var intId = (lastField && lastField.length && lastField.data("idx") + 1);
+        //var newItemInputWrapper = $("<div class=\"form-group\"><input type=\"text\" class=\"form-control-user\" placeholder=\"Item Name\" name=\"items\" id=\"inputItem" + intId + "\"></div>");
+        var newItemInputWrapper = $("<div class=\"form-group\"><input type=\"text\" class=\"form-control-user\" placeholder=\"Item Name\" name=\"items\"></div>");
+        console.log(newItemInputWrapper);
+        //newItemInputWrapper.data("idx", intId);
+        console.log($("#itemTextBoxes").html());
+        $("#itemTextBoxes").append(newItemInputWrapper);
+        console.log($("#itemTextBoxes").html());
+    });
+});
