c5e71292fe40d2dba6fb7f7c2414bde6e69eee23
Antonio Camas <antoniocamas@hotmail.com>
2019-10-26 19:29:24 +0200

    Enable dynamically form submit button

--- src/main/resources/static/js/behavior.js
+++ src/main/resources/static/js/behavior.js
@@ -1,32 +1,66 @@
 function deleteOrderConfirmation(delete_location) {
-    if(confirm("Are you sure you want to delete this entry?")){
+    if (confirm("Are you sure you want to delete this entry?")) {
         document.location = delete_location;
-    }    
+    }
 };
 
 function switchDeleteButtons() {
-    if($("#itemTextBoxes").children().length > 1){
+    if ($("#itemTextBoxes").children().length > 1) {
         $("#itemTextBoxes").find('.btn-danger').show();
     }
-    else{
+    else {
         $("#itemTextBoxes").find('.btn-danger').hide();
     }
 };
 
-$(document).ready(switchDeleteButtons());
+$(document).ready(
+    switchDeleteButtons(),
+    switchForm()
+    );
 
-$(document).ready(function() {
-    $("#addItem").click(function() {
+$(document).ready(function () {
+    $("#addItem").click(function () {
         var newItemInputWrapper = $("#itemTextBoxes").children()[0].outerHTML;
         $("#itemTextBoxes").append(newItemInputWrapper);
         $("#itemTextBoxes").find("input").last().val("");
         switchDeleteButtons();
+        switchForm();
     });
 });
 
 //Since the class rowInputDeleter is added dynamically the listener has to be at document level
-$(document).on('click', '.rowInputDeleter', function(event) {
+$(document).on('click', '.rowInputDeleter', function (event) {
     event.preventDefault();
     $(event.target).closest(".form-group").remove();
-    switchDeleteButtons();    
+    switchDeleteButtons();
+    switchForm();
 });
+
+$(document).on('change','form :input', function (event) {
+    switchForm();
+});
+
+
+function switchForm(){
+    if(validateForm()){
+        $("#btnSubmit").attr("disabled", false);
+    }
+    else{
+        $("#btnSubmit").attr("disabled", true);
+    }
+};
+
+function validateForm() {
+    var empty = false;
+    $('form :input[type=text]').each(function (index, data) {
+        if ($(data).val() == "") {
+            empty = true;
+            return false;
+        }
+        
+    });
+    if (empty){
+        return false;
+    }
+    return true;
+};
